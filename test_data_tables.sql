5d2626c86e7949fd923f793259fd0cd55dcdb80c
Lauren Dulgarian <113635199+dulgarianla@users.noreply.github.com>
2023-10-24 21:28:06 -0600

    Create test_data_tables.sql
    To show user what is in each table

---
+++ test_data_tables.sql
@@ -0,0 +1,27 @@
+-- Retrieve data from the 'college' table
+USE University;
+SELECT * FROM college;
+
+-- Retrieve data from the 'department' table
+USE University;
+SELECT * FROM department;
+
+-- Retrieve data from the 'course' table
+USE University;
+SELECT * FROM course;
+
+-- Retrieve data from the 'faculty' table
+USE University;
+SELECT * FROM faculty;
+
+-- Retrieve data from the 'semester' table
+USE University;
+SELECT * FROM semester;
+
+-- Retrieve data from the 'student' table
+USE University;
+SELECT * FROM student;
+
+-- Retrieve data from the 'enrollment' table
+USE University;
+SELECT * FROM enrollment;
